3eb81268c674fd7bad791f5accd74c8cc0bfe5d5
mushketeers <atosprotosaramisdartanian@gmail.com>
2024-08-14 13:37:58 +0200

    ტესტ

--- leaders test/exam tasks/algo2.py
+++ leaders test/exam tasks/algo2.py
@@ -1,7 +1,13 @@
 def count(str):
+    if not str:
+        return 0
     count = 0
     for i in str:
         if i.isupper:
             count+=1
     return count
 
+'''
+ყველაზე ცოტა მოხდება მაშინ როდესაც სტრინგი ცარიელია მოხდება 0 შემოწმება
+ყველაზე მეტი კი n ანუ თავად სტრინგის სიგრძე
+'''
